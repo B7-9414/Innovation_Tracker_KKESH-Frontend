75d4655af9b3a981d859362b5d89ca649af1b7f0
B7-9414 <banawi.bassam@gmail.com>
2025-05-31 11:21:14 +0300

    deployment for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 
 export default defineConfig({
-  base: "/Innovation_Tracker_KKESH-Frontend/", 
+  base: "/Innovation_Tracker_KKESH-Frontend", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
