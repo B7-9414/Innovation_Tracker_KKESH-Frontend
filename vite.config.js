652ec4ec3cc5c5c247318d4927d997aa9ac16c32
B7-9414 <banawi.bassam@gmail.com>
2025-05-31 10:25:18 +0300

    updated vite confg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
+  base: "/Innovation_Tracker_KKESH-Frontend/", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
